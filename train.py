c39e5e4447d8f14270e7c360dc6f937c7c731db1
miyazaki.carp <miyazaki.carp@gmail.com>
2020-06-27 20:51:38 +0900

    modifying training code

--- train.py
+++ train.py
@@ -14,27 +14,38 @@ from yolo3.utils import get_random_data
 
 
 def _main():
-    annotation_path = 'train.txt'
+    # log & model file save directory
     log_dir = 'logs/000/'
-    classes_path = 'model_data/voc_classes.txt'
+
+    # annotation data text file
+    annotation_path = 'training/annotation_sample.txt'
+
+    # class definition
+    classes_path = 'training/classes_sample.txt'
+
+    # anchor (default)
     anchors_path = 'model_data/yolo_anchors.txt'
+
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
 
-    input_shape = (416,416) # multiple of 32, hw
+    input_shape = (320, 320) # multiple of 32, hw
 
-    is_tiny_version = len(anchors)==6 # default setting
+    is_tiny_version = (len(anchors) == 6) # default setting
     if is_tiny_version:
-        model = create_tiny_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
+        model = create_tiny_model(input_shape, anchors, num_classes, freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
-        model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+        # place h5 file in model_data/
+        # h5 file can be converted by yolov3.cfg and yolov3.weights
+        #   * you have to download yolov3.weights from 
+        #     https://pjreddie.com/media/files/yolov3.weights
+        #   $ wget https://pjreddie.com/media/files/yolov3.weights  
+        model = create_model(input_shape, anchors, num_classes, freeze_body=2, weights_path='model_data/yolo.h5')
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=25)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
@@ -54,16 +65,18 @@ def _main():
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 32
+        batch_size = 16 # default size is 32 but too large for colaboratory training
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=50,
+                epochs = 3,
+                #epochs=50,
                 initial_epoch=0,
-                callbacks=[logging, checkpoint])
-        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
+                #callbacks=[logging, checkpoint])   # if you need logging and checkpoint
+                callbacks=[checkpoint])
+        #model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
@@ -73,16 +86,25 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
+        #batch_size = 2
+        batch_size = 16 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=50,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights(log_dir + 'trained_weights_final.h5')
+            #epochs=100,
+            #initial_epoch=50,
+            epochs=10,
+            initial_epoch=5,
+            #callbacks=[logging, checkpoint, reduce_lr, early_stopping])
+            callbacks=[checkpoint, reduce_lr, early_stopping])
+        #model.save_weights(log_dir + 'trained_weights_final.h5')
+
+        # You have to use save (not save_weights)
+        # because you cannot use the script for converting h5 to coreml
+        # if you use save_weights API.
+        model.save(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
 
