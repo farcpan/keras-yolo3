086488b0677f93ad3a65ccd9c3927e7d5e86d85e
miyazaki.carp <miyazaki.carp@gmail.com>
2020-07-01 20:27:49 +0900

    エポック数変更

--- train.py
+++ train.py
@@ -71,8 +71,7 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs = 3,
-                #epochs=50,
+                epochs=50,
                 initial_epoch=0,
                 #callbacks=[logging, checkpoint])   # if you need logging and checkpoint
                 callbacks=[checkpoint])
@@ -93,10 +92,8 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            #epochs=100,
-            #initial_epoch=50,
-            epochs=10,
-            initial_epoch=5,
+            epochs=100,
+            initial_epoch=50,
             #callbacks=[logging, checkpoint, reduce_lr, early_stopping])
             callbacks=[checkpoint, reduce_lr, early_stopping])
         #model.save_weights(log_dir + 'trained_weights_final.h5')
